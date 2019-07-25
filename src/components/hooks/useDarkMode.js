e5ebfea0dc0e9897f294f5cec3430289f2fa921a
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-25 14:12:51 -0600

    working on useDarkMode function

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -1,4 +1,9 @@
 import React, { useEffect } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 
-const[someValue, setSomeValue] = useLocalStorage('mykey')
+
+export const useDarkMode = () => {
+    const[someValue, setSomeValue] = useLocalStorage('mykey')
+
+
+}
